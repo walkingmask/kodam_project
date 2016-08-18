ab3979d4783869c9ffa4604398fcdd9e5619ed99
walkingmask <walkingmask@gmail.com>
2016-08-18 14:12:32 +0900

    added test comment

--- kodama_app/iOS/Main.cs
+++ kodama_app/iOS/Main.cs
@@ -14,6 +14,8 @@ namespace kodama_app.iOS
 		{
 			// if you want to use a different Application Delegate class from "AppDelegate"
 			// you can specify it here.
+
+			// test comment
 			UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
