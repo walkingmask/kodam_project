a2862e317ab655e98f8ecce4bd512fcd65d80eff
walkingmask <walkingmask@gmail.com>
2016-08-25 18:33:26 +0900

    プロジェクトのクリーンアップ
    プロトタイプの開発をしやすくするために開発環境をXamarinからXcode(Swift)に変更
    それに伴って
    Xcodeプロジェクトを作成
    Xamarinプロジェクトを削除
    練習ファイル/ディレクトリを削除
    gitignoreを変更

---
+++ kodama-app/kodama-appTests/kodama_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  kodama_appTests.swift
+//  kodama-appTests
+//
+//  Created by kazuki nagamine on 8/25/16.
+//  Copyright © 2016 kodama-project. All rights reserved.
+//
+
+import XCTest
+@testable import kodama_app
+
+class kodama_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
